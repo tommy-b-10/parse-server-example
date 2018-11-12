025e56ed882f9b25fbc487529febe1c31acc8ebc
tommy-b-10 <tommy-b-10@hotmail.com>
2018-11-12 14:12:31 +1030

    Test

--- cloud/main.js
+++ cloud/main.js
@@ -3,12 +3,12 @@ Parse.Cloud.define('hello', function(req, res) {
   var userId = req.params.userId;
   console.log(userId);
   var queryUser = new Parse.Query(Parse.User);
-  queryUser.get(userId, { useMasterKey:true,
-      success: function(user) {
-          response.success(user.get("apiKey"));
-      },
-      error: function(error) {
-          response.success("fail");
-      }
-   });
+  queryUser.get(userId, { useMasterKey:true }).then(function(user) {
+      var key = user.get("apiKey");
+      return key;
+    })
+    .catch(function(error) {
+      console.error("Got an error " + error.code + " : " + error.message);
+      return "error";
+    });
 });
