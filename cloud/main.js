8ee704df1968e81d4de07a66d569d47bb0c31a61
tommy-b-10 <tommy-b-10@hotmail.com>
2018-11-12 13:54:53 +1030

    Test

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,6 @@
 
 Parse.Cloud.define('hello', function(req, res) {
-  var user = req["userId:];
+  var user = req["userId"];
   console.log(user);
   var result = ["key":"tbrereton9"];
   res.success(result);
