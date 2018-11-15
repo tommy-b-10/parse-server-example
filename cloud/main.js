a199a536eb36d0a63c2782c2a9fef272204cfa4b
tommy-b-10 <tommy-b-10@hotmail.com>
2018-11-15 18:23:33 +1030

    test

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ Parse.Cloud.define("verifyToken", async (req) => {
   var userId = req.params.userId;
   var userQuery = new Parse.Query(Parse.User);
   userQuery.equalTo("objectId", userId);
-  userQuery.first({ useMasterKey:true }).then(function(user) {
+  var result = userQuery.first({ useMasterKey:true }).then(function(user) {
     var key = user.get("apiKey");
     return key;
   })
@@ -11,13 +11,20 @@ Parse.Cloud.define("verifyToken", async (req) => {
     console.error("Got an error " + error.code + " : " + error.message);
     return "error";
   });
+  return result;
 });
 
 Parse.Cloud.define("beginStream", async (req) => {
   var userId = req.params.userId;
   var userQuery = new Parse.Query(Parse.User);
   userQuery.equalTo("objectId", userId);
-  userQuery.first({ useMasterKey:true }).then(function(user) {
+  let user = userQuery.first({ useMasterKey:true }).then(function(user) {
+    return user;
+  }).catch(function(error) {
+    console.error("Got an error " + error.code + " : " + error.message);
+    return "error";
+  });
+  if (user != "error") {
     var Post = Parse.Object.extend("Post");
     var post = new Post();
     post.set("user", user);
@@ -25,15 +32,15 @@ Parse.Cloud.define("beginStream", async (req) => {
     post.set("isLive", True);
     post.set("isVisibleAt", new Date());
     post.set("mediaType", "live");
-    post.save().then((post) => {
+    let result = post.save().then((post) => {
       console.log("Created post with Id: " + post.id);
       return "success";
     }, (error) => {
       console.log("Error creating post! " + error.message);
       return "error";
     });
-  }).catch(function(error) {
-    console.error("Got an error " + error.code + " : " + error.message);
-    return "error";
-  });
+    return result;
+  } else {
+    return "error";  
+  }
 });
