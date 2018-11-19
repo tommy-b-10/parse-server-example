ce26dc5dc1314ee455d394e3cd7fca2042bd0a97
tommy-b-10 <tommy-b-10@hotmail.com>
2018-11-19 14:58:48 +1030

    test

--- cloud/main.js
+++ cloud/main.js
@@ -65,8 +65,40 @@ Parse.Cloud.define("endStream", async (req) => {
 		for (var i = 0; i < posts.length; i++) {
 			var post = posts[i];
 			post.set("isLive", false);
-			post.unset("liveKey");
 			post.set("mediaType", "dvr");
+			post.save();
+		}
+		return "success";
+	}).catch(function(error) {
+		console.error("Got an error " + error.code + " : " + error.message);
+		return "error";
+    });
+    return res;
+  }).catch(function(error) {
+    console.error("Got an error " + error.code + " : " + error.message);
+    return "error";
+  });
+  return result;
+});
+
+Parse.Cloud.define("addStreamDVR", async (req) => {
+  var userId = req.params.userId;
+  var streamKey = req.params.streamKey;
+  var vodURL = req.params.vodURL;
+  var vodKey = vodURL.split(streamKey)[1];
+  var userQuery = new Parse.Query(Parse.User);
+  userQuery.equalTo("objectId", userId);
+  var result = userQuery.first({ useMasterKey:true }).then(function(user) {
+    var Post = Parse.Object.extend("Post");
+    var postQuery = new Parse.Query(Post);
+    postQuery.equalTo("user", user);
+    postQuery.equalTo("isLive", false);
+	postQuery.equalTo("liveKey", streamKey);
+    postQuery.equalTo("mediaType", "dvr");
+    var res = postQuery.find({ useMasterKey:true }).then(function(posts) {
+		for (var i = 0; i < posts.length; i++) {
+			var post = posts[i];
+			post.set("liveKey", vodKey);
 			post.save();
 		}
 		return "success";
